a88467db86d940241b27a493f8d6a3a2ca68308e
ManuelCebreiro <cebreirom@gmail.com>
2022-06-29 20:35:38 +0000

    ejercicio en proceso.1

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 const Footer = (props) => {
     return(
-    <div
-         className="text-center p-3"
-         style={{backgroundColor: "grey"}}>
-      © 2020 Copyright:
+        <div class="p-3 mb-2 bg-dark text-white text-center p-3">
+              Copyright ©
         <a className="text-white" href="https://mdbootstrap.com/"
-         >MDBootstrap.com</a>
+         >Your Website 2019</a>
     </div>
     )
 }
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,36 +2,25 @@ import React from "react";
 
 const Navbar = (props) => {
     return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarText">
-        <span className="navbar-text">
-            Start Bootstrap
-        </span>
-        <ul className="navbar-nav">
-            <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#">Services</a>
-            </li>
-            
-            <li className="nav-item navbar-rigth">
-                <a className="nav-link" href="#">Contact</a>
-            </li>
-        </ul>
-        <span className="navbar-text">
-      Navbar text with an inline element
-    </span>
-    </div>
-    </nav>
-    )
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark ps-5">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">dsdsds</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon">dsdsd</span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div className="navbar-nav ">
+              <a className="nav-link active" aria-current="page" href="#">sdsdsdsdsds</a>
+              <a className="nav-link" href="#">sdsdsd</a>
+              <a className="nav-link" href="#">dsdsdsd</a>
+              <a className="nav-link" href="#">dssdsd</a>
+            </div>
+            </div>
+          </div>
+          </nav>
+  
+      )
 }
 
 export default Navbar

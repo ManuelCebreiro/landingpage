58d3f2ec9cc1e71668d248485d75ce3ce12abebf
ManuelCebreiro <cebreirom@gmail.com>
2022-07-06 18:44:08 +0000

    Proyecto realizado

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className="container d-flex justify-content-evenly pt-4 col col-sm-6 col-lg-3 mb-5">
-    <div className="ard" style={{width: "18rem"}}>
+    <div className="card" style={{width: "18rem"}}>
     <img className="card-img-top" src="http://via.placeholder.com/500x325/" alt="Card image cap" /> 
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
+      <a href="#" className="btn btn-primary">Find Out More!</a>
     </div>
   </div>
   </div>
--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 const Footer = (props) => {
     return(
-        <div className="navbar navbar-dark navbar-expand-sm bg-dark mb-4 d-flex justify-content-center text-white mt-5 p-3"style={{height: "10vh"}} >
-     
+        <div id="footer" className="navbar navbar-dark navbar-expand-sm bg-dark d-flex justify-content-center text-white mt-5 mb-0"style={{height: "10vh"}} >
         <p className=" text-white p-1"> Copyright © Your Website 2019</p>
         </div>
     )
